fcbbeb57544bf085c77fb1fd9e612cc48b672e63
Drew <andrew.maring@gmail.com>
2019-08-12 21:44:59 +0000

    Add base_model.py, city.py, state.py

--- models/state.py
+++ models/state.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import os
 
 
-class State(BaseModel):
+Base = declarative_base()
+
+class State(Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+
+    id = Column(Integer,
+                unique=True,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False)
+
+    name = Column(String(128),
+                  nullable=False)
+
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete-orphan")
+
+    @property
+    def cities():
+        _list = []
+        for city in self.cities:
+            if city.state_id == self.id:
+                _list.append(city)
+        return(_list)
