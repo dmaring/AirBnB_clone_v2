6abcd799527e04fdbc78294ac29f155cea71da61
Tu Vo <tuvo1106@gmail.com>
2019-08-13 22:15:56 +0000

    Change null condition on users

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy.org import relationship
 
 
-class Amenity(BaseModel):
+association_table = Table('association', Base.metadata,
+        Column(),
+        Column()
+        )
+
+
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = "amenities"
+
+    name = Column(String(128),
+                  nullable=False)
+
+    place_amenities = 
