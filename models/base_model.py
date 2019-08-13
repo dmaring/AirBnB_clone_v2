a13346541064a7c55be2a9b911c8c71698d7281f
Tu Vo <tuvo1106@gmail.com>
2019-08-13 22:05:48 +0000

    Uncomment Datetime serialization

--- models/base_model.py
+++ models/base_model.py
@@ -78,8 +78,8 @@ class BaseModel:
         """
         my_dict = dict(self.__dict__)
         my_dict["__class__"] = str(type(self).__name__)
-        # my_dict["created_at"] = self.created_at.isoformat()
-        # my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
         if "_sa_instance_state" in my_dict:
             del(my_dict['_sa_instance_state'])
         return my_dict
