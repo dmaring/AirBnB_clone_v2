76543badd6557f4b998bcbeb82a2723c3594f1c4
Drew <andrew.maring@gmail.com>
2019-08-19 21:18:21 +0000

    Add 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Python fabric module to archive a static website into .tgz file
+"""
+import os
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Function that archives a version of a static site into tarball
+    """
+
+    if not os.path.exists('versions'):
+        os.makedirs('versions')
+    curr_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    name_time = "versions/web_static_{}.tgz".format(curr_time)
+    command = "tar -czvf {} web_static".format(name_time)
+    result = local(command)
+    if result.failed:
+        return(None)
+    else:
+        return(name_time)
