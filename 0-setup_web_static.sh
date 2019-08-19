7daaaf2ce66a3efb499f1e78007f841dacef0b8d
Drew <andrew.maring@gmail.com>
2019-08-19 19:51:11 +0000

    Make 0-setup_web_static.sh executable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,6 @@
 #a Bash script that sets up web servers for the deployment of web_static
 
 apt update && apt install -y nginx
-mkdir --parents /data/web_static/releases/
 mkdir --parents /data/web_static/shared/
 mkdir --parents /data/web_static/releases/test/
 cat > /data/web_static/releases/test/index.html <<EOF
