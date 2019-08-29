fb53d0f3120e29c151031258525994154de06714
Drew <andrew.maring@gmail.com>
2019-08-29 19:37:05 +0000

    Implement strict_slashes=False

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=None)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ A route that displays Hello HBNB! """
     return "Hello HBNB!"
