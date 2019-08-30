e4978ccc334ba96899d285fc3e5a5b58fec148bb
Drew <andrew.maring@gmail.com>
2019-08-30 19:05:12 +0000

    Add 7-states_list.py 7-states_list.hmtl

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+Flask module that returns a Flask app
+"""
+from flask import Flask, escape, render_template
+from models import storage
+
+app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+app.url_map.strict_slashes = False
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ A route that displays Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ A route that displays HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ A route that displays C and input text """
+    return "C %s" % escape(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ A route that displays python and input text """
+    return "Python %s" % escape(text.replace("_", " "))
+
+
+@app.route('/number/<int:number>', strict_slashes=False)
+def is_number(number):
+    """ A route that displays input number only if an int is input """
+    return "%s is a number" % escape(number)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def is_number_template(n):
+    """ A route that displays a template and number only if an int is input """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    """ A route that displays a template and number only if an int is input """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.route('/states_list')
+def states_list():
+    """ A route that displays a template and number only if an int is input """
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database at the end of the request."""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run()
